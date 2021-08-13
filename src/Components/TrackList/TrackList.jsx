acd0cf7c2e3d08cbf8e4ed5bea8afe080c4dbff1
Helena Nel <mailhedri@gmail.com>
2021-08-13 15:17:39 +0200

    commit

--- src/Components/TrackList/TrackList.jsx
+++ src/Components/TrackList/TrackList.jsx
@@ -7,7 +7,7 @@ import {
   dislikeSongAction,
 } from "../../redux/actions/actions.js";
 import { HeartFill, Heart } from "react-bootstrap-icons";
-
+import "./TrackList.css";
 const mapStateToProps = (state) => state;
 const mapDispatchToProps = (dispatch) => ({
   likeSong: (songObj) => dispatch(likeSongAction(songObj)),
@@ -23,28 +23,16 @@ class TrackList extends React.Component {
             <Col className="text-center" md={1}>
               #
             </Col>
-            <Col md={10}>TITLE</Col>
+            <Col md={9}>TITLE</Col>
             <Col className="text-center" md={1}>
               <i className="far fa-clock"></i>
             </Col>
+            <Col></Col>
           </Row>
           {this.props.tracks.map((track, i) => (
             <Row key={track.id} className="py-2 text-white">
-              <Col style={{ width: "20px" }}>
-                {this.props.tracks.find((_tracks) => track.id === track.id) ? (
-                  <HeartFill
-                    id="heartfill"
-                    onClick={() =>
-                      this.props.dislikeSong(this.props.likes.songs)
-                    }
-                  />
-                ) : (
-                  <Heart
-                    id="heart"
-                    onClick={() => this.props.likeSong(this.props.likes.songs)}
-                  />
-                )}
-              </Col>
+              {console.log("track id", track.id)}
+
               <Col className="text-center" md={1}>
                 <p className="mx-2 my-0 text-muted">{i + 1}</p>
               </Col>
@@ -53,6 +41,21 @@ class TrackList extends React.Component {
               </Col>
               <Col className="text-center" md={1}>
                 <p className="my-0 text-muted">{secsToMins(track.duration)}</p>
+              </Col>
+              <Col style={{ width: "20px" }}>
+                {this.props.likes.songs.find(
+                  (_track) => _track.id === this.props.track.id
+                ) ? (
+                  <HeartFill
+                    id="heartfill"
+                    onClick={() => this.props.dislikeSong(this.props.track)}
+                  />
+                ) : (
+                  <Heart
+                    id="heart"
+                    onClick={() => this.props.likeSong(this.props.track)}
+                  />
+                )}
               </Col>
             </Row>
           ))}
--- src/redux/reducers/likes.js
+++ src/redux/reducers/likes.js
@@ -1,17 +1,19 @@
-import * as types from "../types/types"
-import { initialState } from "../store"
+import * as types from "../types/types";
+import { initialState } from "../store";
 
 const likesReducer = (state = initialState.likes, action) => {
   switch (action.type) {
     case types.LIKE_SONG:
-      return { ...state, songs: [...state.songs, action.payload] }
+      return { ...state, songs: [...state.songs, action.payload] };
 
     case types.DISLIKE_SONG:
-      return { ...state, songs: state.songs.filter(song => song.id !== action.payload) }
-
+      const modifiedFavs = [
+        { ...state.songs.filter((song) => song.id !== action.payload.id) },
+      ];
+      return { ...state, songs: modifiedFavs };
     default:
-      return state
+      return state;
   }
-}
+};
 
-export default likesReducer
+export default likesReducer;
