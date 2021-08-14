32bf29081427f4823f33bf6c1c0bdb4a9c578c54
Tiago Brandao <tiagofpbrandao@gmail.com>
2021-08-14 12:44:30 +0100

    Minor fixes to player material

--- src/Components/MediaCards/TrackCard.jsx
+++ src/Components/MediaCards/TrackCard.jsx
@@ -1,21 +1,21 @@
-import { Card, Col } from "react-bootstrap";
-import { Link } from "react-router-dom";
-import { secsToMins } from "../../assets/helpers";
-import { HeartFill, Heart } from "react-bootstrap-icons";
-import { connect } from "react-redux";
+import { Card, Col } from "react-bootstrap"
+import { Link } from "react-router-dom"
+import { secsToMins } from "../../assets/helpers"
+import { HeartFill, Heart } from "react-bootstrap-icons"
+import { connect } from "react-redux"
 import PlayPauseBtn from "../PlayPauseButton/PlayPauseBtn"
 
-import { likeSongAction, dislikeSongAction } from "../../redux/actions/actions";
+import { likeSongAction, dislikeSongAction } from "../../redux/actions/actions"
 
-import "./ContentCard.css";
+import "./ContentCard.css"
 
-const mapStateToProps = (state) => state;
-const mapDispatchToProps = (dispatch) => ({
-  likeSong: (songObj) => dispatch(likeSongAction(songObj)),
-  dislikeSong: (songId) => dispatch(dislikeSongAction(songId)),
-});
+const mapStateToProps = state => state
+const mapDispatchToProps = dispatch => ({
+  likeSong: songObj => dispatch(likeSongAction(songObj)),
+  dislikeSong: songId => dispatch(dislikeSongAction(songId)),
+})
 
-const TrackCard = (props) => {
+const TrackCard = props => {
   return (
     <Col className="mb-4" xs={12} sm={6} md={4} lg={3}>
       <Card className="h-100 bg-transparent border-0 content-card">
@@ -24,45 +24,36 @@ const TrackCard = (props) => {
           <PlayPauseBtn songObj={props.trackObj} />
         </div>
         <Card.Body className="p-2 bg-transparent">
-          <Card.Title className="m-0 text-center text-white">
-            {props.trackObj.title}
-          </Card.Title>
-          <Link
-            className="text-muted"
-            to={`/album?id=${props.trackObj.album.id}`}
-          >
-            <Card.Text className="m-0 text-center">
-              {props.trackObj.album.title}
-            </Card.Text>
+          <Card.Title className="m-0 text-center text-white">{props.trackObj.title}</Card.Title>
+          <Link className="text-muted" to={`/album?id=${props.trackObj.album.id}`}>
+            <Card.Text className="m-0 text-center">{props.trackObj.album.title}</Card.Text>
           </Link>
-          <Link
-            className="text-muted"
-            to={`/artist?id=${props.trackObj.artist.id}`}
-          >
-            <Card.Text className="m-0 text-center">
-              {props.trackObj.artist.name}
-            </Card.Text>
+          <Link className="text-muted" to={`/artist?id=${props.trackObj.artist.id}`}>
+            <Card.Text className="m-0 text-center">{props.trackObj.artist.name}</Card.Text>
           </Link>
           <Card.Text className="m-0 text-center">
             {secsToMins(props.trackObj.duration)}
-            {props.likes.songs.find((_trackObj) => {
+            {props.likes.songs.find(_trackObj => {
+              /**
+               *
+               *   if(likes[song.id]){
+               *    likes[song.id]=false
+               * }
+               * else{
+               * likes[song.id]=true
+               * }
+               */
               // console.log("track id", props.trackObj.id);
-              return _trackObj.id === props.trackObj.id;
+              return _trackObj.id === props.trackObj.id
             }) ? (
-              <HeartFill
-                id="heartfill"
-                onClick={() => props.dislikeSong(props.trackObj.id)}
-              />
+              <HeartFill id="heartfill" onClick={() => props.dislikeSong(props.trackObj.id)} />
             ) : (
-              <Heart
-                id="heart"
-                onClick={() => props.likeSong(props.trackObj)}
-              />
+              <Heart id="heart" onClick={() => props.likeSong(props.trackObj)} />
             )}
           </Card.Text>
         </Card.Body>
       </Card>
     </Col>
-  );
-};
-export default connect(mapStateToProps, mapDispatchToProps)(TrackCard);
+  )
+}
+export default connect(mapStateToProps, mapDispatchToProps)(TrackCard)
